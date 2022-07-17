e6decb3006a32185d0e33d4265adce5f403f4e8d
noguchi-daiki <daiki.noguchi@excite.jp>
2022-07-17 17:23:19 +0900

    [modify] コメント外し忘れと作成時のtimestampの現在日時をデフォルトに変更

--- src/laravel-project/database/migrations/2022_07_17_061620_create_staff_table.php
+++ src/laravel-project/database/migrations/2022_07_17_061620_create_staff_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 class CreateStaffTable extends Migration
 {
@@ -17,17 +18,18 @@ class CreateStaffTable extends Migration
             $table->id();
             $table->bigInteger('company_id')->unsigned();
             $table->bigInteger('user_id')->unsigned();
-            $table->timestamps();
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
 
-            // $table->foreign('company_id')
-            //     ->references('id')
-            //     ->on('companies')
-            //     ->onDelete('cascade');
+            $table->foreign('company_id')
+                ->references('id')
+                ->on('companies')
+                ->onDelete('cascade');
 
-            // $table->foreign('user_id')
-            //     ->references('id')
-            //     ->on('users')
-            //     ->onDelete('cascade');
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
         });
     }
 
