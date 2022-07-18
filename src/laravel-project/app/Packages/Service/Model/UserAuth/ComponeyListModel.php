003516c0f55f3750313ccfed0a1239b636420c73
noguchi-daiki <daiki.noguchi@excite.jp>
2022-07-18 20:24:35 +0900

    [add] 会社Model

---
+++ src/laravel-project/app/Packages/Service/Model/UserAuth/ComponeyListModel.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Packages\Service\Model\UserAuth;
+
+class ComponeyListModel
+{
+    protected $companyId;
+    protected $companyName;
+    protected $companyAddress;
+    protected $createdAt;
+
+    public function __construct(
+        ?int $companyId,
+        string $companyName,
+        string $companyAddress,
+        string $createdAt
+    ) {
+        $this->companyId=$companyId;
+        $this->companyName=$companyName;
+        $this->companyAddress=$companyAddress;
+        $this->createdAt=$createdAt;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getCompanyId(): ?int
+    {
+        return $this->companyId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCompanyName(): string
+    {
+        return $this->companyName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCompanyAddress(): string
+    {
+        return $this->companyAddress;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCreatedAt(): string
+    {
+        return $this->createdAt;
+    }
+}
