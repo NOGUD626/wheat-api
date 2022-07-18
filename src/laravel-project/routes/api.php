a8405a43bc31e6faaae188da11710781f5f367ac
NOGUD626 <89671687+NOGUD626@users.noreply.github.com>
2022-07-18 10:57:56 +0900

    [modify] singinのapiのエンドポイント変更

--- src/laravel-project/routes/api.php
+++ src/laravel-project/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\UserAuth\UserSignInController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,5 +20,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::middleware(['firebase'])->group(function (){
-    Route::post('/user', UserController::class);
+    Route::post('/sighin', UserSignInController::class);
 });
