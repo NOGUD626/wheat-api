713138090a847ab61b327a0894812a9d66eb2ec1
NOGUD626 <89671687+NOGUD626@users.noreply.github.com>
2022-08-03 21:15:30 +0900

    [add] form関係のapiの追加

--- src/laravel-project/routes/api.php
+++ src/laravel-project/routes/api.php
@@ -4,6 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserAuth\UserSignInController;
 use App\Http\Controllers\UserAuth\UserLoginController;
+use App\Http\Controllers\Forms\FormGetController;
+use App\Http\Controllers\Forms\FormPostController;
+use App\Http\Controllers\Forms\FormDeleteController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,13 +23,13 @@ use App\Http\Controllers\UserAuth\UserLoginController;
 //     return $request->user();
 // });
 
-Route::group(['prefix' => 'user', 'middleware' => ['auth:sanctum'] ], function () {
-    Route::get('/', function (Request $request) {
-        return $request->user();
-    });
-});
-
 Route::middleware(['firebase'])->group(function (){
     Route::post('/signin', UserSignInController::class);
     Route::post('/login', UserLoginController::class);
+});
+
+Route::group(['prefix' => 'forms', 'middleware' => ['auth:sanctum'] ], function () {
+    Route::get('/', FormGetController::class);
+    Route::post('/', FormPostController::class);
+    Route::delete('/', FormDeleteController::class);
 });
