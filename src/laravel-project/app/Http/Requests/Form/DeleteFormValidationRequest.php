817466410ef724d95691ecdc10db8b90e86d3e8b
NOGUD626 <89671687+NOGUD626@users.noreply.github.com>
2022-08-04 23:41:09 +0900

    [add] form apiのdeleteのバリデーションチェックのrequestクラス追加

---
+++ src/laravel-project/app/Http/Requests/Form/DeleteFormValidationRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\Form;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
+
+class DeleteFormValidationRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            "formId" => ['required', 'regex:/^(FM\-)+(\w){13}$/'],
+            'companyId' => ['required', 'regex:/^(C\-)+(\w){13}$/']
+        ];
+    }
+
+    protected function failedValidation(Validator $validator): void
+    {
+        $response = response()->json([
+            'status' => 'validation error',
+            'errors' => $validator->errors()
+        ], 400);
+        throw new HttpResponseException($response);
+    }
+}
