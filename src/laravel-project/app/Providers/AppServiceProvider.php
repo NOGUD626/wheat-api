bec65cd6d6e657e2cfb103c450647eaaefb9df47
NOGUD626 <89671687+NOGUD626@users.noreply.github.com>
2022-08-04 14:32:45 +0900

    [add] formapiのrepository層とservice層の追加

--- src/laravel-project/app/Providers/AppServiceProvider.php
+++ src/laravel-project/app/Providers/AppServiceProvider.php
@@ -9,10 +9,14 @@ use App\Packages\Component\Transaction\DbTransaction;
 // Service
 use App\Packages\Service\Application\UserAuth\UserLoginService;
 use App\Packages\Service\Application\UserAuth\UserLoginServiceInterface;
+use App\Packages\Service\Application\Form\FormSchemaService;
+use App\Packages\Service\Application\Form\FormSchemaServiceInterface;
 
 // Repository
 use App\Packages\Repository\Application\UserAuth\UserLoginRepository;
 use App\Packages\Repository\Application\UserAuth\UserLoginRepositoryInterface;
+use App\Packages\Repository\Application\Form\FormSchemaRepository;
+use App\Packages\Repository\Application\Form\FormSchemaRepositoryInterface;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,7 +30,8 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(TransactionInterface::class, DbTransaction::class);
         $this->app->bind(UserLoginServiceInterface::class, UserLoginService::class);
         $this->app->bind(UserLoginRepositoryInterface::class, UserLoginRepository::class);
-        
+        $this->app->bind(FormSchemaServiceInterface::class, FormSchemaService::class);
+        $this->app->bind(FormSchemaRepositoryInterface::class, FormSchemaRepository::class);
     }
 
     /**
